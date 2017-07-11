bd310343e8f24fee8117a27f967613a516f82ec4
Raphael Guntersweiler <raphael@rgunti.ch>
2017-07-12 00:46:19 +0200

    /api/shutdown and /api/reboot implemented

--- routes/routes_api.js
+++ routes/routes_api.js
@@ -23,14 +23,55 @@
  * ********************************************************************************* */
 
 const debug = require('debug')('node-system-control:Routes/API');
+const sshDebug = require('debug')('node-system-control:SSH');
 const config = require('../base/config');
+const ResponseUtils = require('../base/response_utils');
+
+var SSH = require('simple-ssh');
 
 var express = require('express');
 var router = express.Router();
+
+function getSSH() {
+    var ssh = new SSH(getSSHConfig());
+    ssh.on('error', function(err) {
+        sshDebug('Oops, something went wrong.');
+        sshDebug(err);
+        ssh.end();
+    });
+    return ssh;
+}
+
+function getSSHConfig() {
+    return {
+        host: config.getValue(config.KEYS.SERVER_HOST),
+        user: config.getValue(config.KEYS.SERVER_USER),
+        pass: config.getValue(config.KEYS.SERVER_PASS)
+    };
+}
 
 /* GET "/" */
 router.get('/', function(req, res, next) {
     res.json({ ok: true });
 });
+
+/* POST "Shutdown" */
+router.post('/shutdown', function(req, res, next) {
+    var ssh = getSSH();
+    ssh.exec('sudo halt', { pty: true, out: function(o) { sshDebug(o) } }).start();
+    ResponseUtils.sendEmptyResponse(res);
+});
+
+/* POST "Reboot" */
+router.post('/reboot', function(req, res, next) {
+    var ssh = getSSH();
+    ssh.exec('sudo reboot', { pty: true, out: function(o) { sshDebug(o) } }).start();
+    ResponseUtils.sendEmptyResponse(res);
+});
+
+// -- -- --
+// Check if SSH can be reached
+var testSSH = getSSH();
+testSSH.exec('echo Test Connection succeeded', { out: function(o) { sshDebug(o) } }).start();
 
 module.exports = router;
